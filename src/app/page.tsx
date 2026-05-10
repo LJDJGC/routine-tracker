5e772dc57249897c925d7c630a974bfb9fe71123
LJDJGC <singsongsmusic@gmail.com>
2026-05-10 12:09:50 +0900

    feat/handson: transcribe session form implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,7 +60,10 @@ export default function Home() {
             <label htmlFor="type" className="mb-2 block text-sm font-medium text-gray-700 dark:text-zinc-300">
               Type
             </label>
-            <select name="type" id="type" className="w-full rounded-md border border-gray-300 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-zinc-700 dark:bg-zinc-800 dark:text-white">
+            {/*2. onChangeハンドラー:
+            ユーザーが入力を変えるたびに、(e) => setNewTypw(...) が実行され、
+            Reactの状態(newType)を最新の入力値で更新します。*/}
+            <select name="type" id="type" value={newType} onChange={(e) => setNewType(e.target.value as Session['type'])} className="w-full rounded-md border border-gray-300 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-zinc-700 dark:bg-zinc-800 dark:text-white">
               <option value="study">Study</option>
               <option value="workout">Workout</option>
             </select>
@@ -70,21 +73,21 @@ export default function Home() {
             <label htmlFor="duration" className="mb-2 block text-sm font-medium text-gray-700 dark:text-zinc-300">
               Duration (minutes)
             </label>
-            <input type="number" id="duration" name="duration" min="1" className="w-fukk rounded-md border border-gray-300 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-zinc-700 dark:bg-zinc-800 dark:text-white" />
+            <input type="number" id="duration" name="duration" min="1" value={newDuration} onChange={(e) => setNewDuration(Number(e.target.value))} className="w-fukk rounded-md border border-gray-300 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-zinc-700 dark:bg-zinc-800 dark:text-white" />
           </div>
 
           <div className="mb-4">
             <label htmlFor="date" className="mb-2 block text-sm font-medium text-gray-700 dark:text-zinc-300">
               Date
             </label>
-            <input type="date" name="date" id="date" className="w-full rounded-md border border-gray-300 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-zinc-700 dark:bg-zinc-800 dark:text-white" />
+            <input type="date" name="date" id="date" value={newDate} onChange={(e) => setNewDate(e.target.value)} className="w-full rounded-md border border-gray-300 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-zinc-700 dark:bg-zinc-800 dark:text-white" />
           </div>
 
           <div className="mb-6">
             <label htmlFor="note" className="mb-2 block text-sm font-medium text-gray-700 dark:text-zinc-300">
               Note
             </label>
-            <textarea name="note" id="note" rows={3} className="w-full rounded-md border border-gray-300 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-zinc-700 dark:bg-zinc-800 dark:text-white"></textarea>
+            <textarea name="note" id="note" rows={3} value={newNote} onChange={(e) => setNewNote(e.target.value)} className="w-full rounded-md border border-gray-300 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-zinc-700 dark:bg-zinc-800 dark:text-white"></textarea>
           </div>
 
           <button type="submit" className="w-full rounded-md bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:bg-blue-700 dark:hover:bg-blue-800">
@@ -93,7 +96,7 @@ export default function Home() {
         </form>
 
         <div className="space-y-4">
-          {dummySessions.map((session) => (
+          {sessions.map((session) => (
             <div
               key={session.id}
               className={`rounded-xl border-l-8 bg-white p-6 shadow-sm transition-shadow hover:shadow-md dark:bg-zinc-900 ${session.type === "study"
